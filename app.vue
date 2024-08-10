68f46b59e8f89d653f810f23e83f871c12f0bb1a
Nic_ <36131101+NicCraver@users.noreply.github.com>
2024-08-10 17:01:53 +0800

    feat: 添加主题颜色切换功能

--- app.vue
+++ app.vue
@@ -1,11 +1,34 @@
 <script setup lang="ts">
 import { appName } from '~/constants'
 
+const color = useColorMode()
+
 useHead({
   title: appName,
   link: [
     { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' },
   ],
+  meta: [{
+    id: 'theme-color',
+    name: 'theme-color',
+    content: () => color.value === 'dark' ? '#222222' : '#ffffff',
+  }],
+})
+
+useHead({
+  meta: [{
+    id: 'theme-color',
+    name: 'theme-color',
+    content: () => color.value === 'dark' ? '#222222' : '#ffffff',
+  }],
+})
+
+// function toggleDark() {
+//   color.preference = color.value === 'dark' ? 'light' : 'dark'
+// }
+
+onMounted(() => {
+  color.preference = 'light'
 })
 </script>
 
